28ef0505f80a0776fb23ce8b7f30bf57d352d93a
Lee Looi Le <116087284+LooiLe@users.noreply.github.com>
2026-05-07 08:34:12 +0700

    backend first test

---
+++ backend/maps/src/main/java/com/radach/maps/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.radach.maps.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
