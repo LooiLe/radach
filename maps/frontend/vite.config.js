5efce43748a10b991f947f9d6a5e8a44482e4b61
Lee Looi Le <116087284+LooiLe@users.noreply.github.com>
2026-06-08 11:34:39 +0700

    Update vite.config.js

--- maps/frontend/vite.config.js
+++ maps/frontend/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ['springs-designed-cases-div.trycloudflare.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
